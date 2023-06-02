4a988940ccbb5410f964e91b1f0c97d8479b9d54
AndrewNguyen-05 <21520144@gm.uit.edu.vn>
2023-06-02 15:36:45 +0700

    More fancy pics incoming~

--- BPMS/GUI/FormLogin.Designer.cs
+++ BPMS/GUI/FormLogin.Designer.cs
@@ -109,10 +109,10 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(696, 60);
+            this.pictureBox1.Location = new System.Drawing.Point(676, 60);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(738, 229);
+            this.pictureBox1.Size = new System.Drawing.Size(770, 260);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
