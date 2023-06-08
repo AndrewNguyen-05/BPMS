528a168dee880953101920e301e5380787d1e9fa
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-06-08 16:37:54 +0700

    do the same for export

--- BPMS/DAO/ExportReportDAO.cs
+++ BPMS/DAO/ExportReportDAO.cs
@@ -124,5 +124,61 @@ namespace BPMS.DAO
             return list.ToList();
 
         }
+        public DateTime GetNext(DateTime current, int GroupBy)
+        {
+            switch (GroupBy)
+            {
+                case 0:
+                    return current.Add(-current.TimeOfDay).AddDays(1);
+                case 1:
+                    return current.Add(-current.TimeOfDay).AddDays(-current.Day + 1).AddMonths(1);
+                case 2:
+                    return current.Add(-current.TimeOfDay).AddDays(-current.Day + 1).AddMonths(-current.Month + 1).AddYears(1);
+
+            }
+            return current;
+        }
+        /// <summary>
+        /// GroupBy mode: 0 - day, 1 - month, 2 - year
+        /// </summary>
+        public List<int> GetNumberOfExportedBook(DateTime startDate, DateTime endDate, int GroupBy)
+        {
+            List<int> result = new List<int>();
+            //By month
+            for (DateTime start = startDate;
+                 start < endDate;
+                 start = GetNext(start, GroupBy)
+                )
+            {
+                DateTime end = GetNext(start, GroupBy) < endDate ? GetNext(start, GroupBy) : endDate;
+                end = end.AddMilliseconds(-1);
+                var list = from ir in db.ExportReports
+                           join ird in db.ExportReportDetails
+                           on ir.id equals ird.idExport
+                           where start < ir.ExportDate && ir.ExportDate < end
+                           select ird.quantity;
+                result.Add(Enumerable.Sum(list));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Get last number - mode of imported book
+        /// mode 0 - day, 1 - month, 2 - year
+        /// GroupBy mode: 0 - day, 1 - month, 2 - year
+        /// </summary>
+        public List<int> GetNumberOfExportedBook(int number, int mode, int GroupBy)
+        {
+            switch (mode)
+            {
+                case 0:
+                    return GetNumberOfExportedBook(DateTime.Now.AddDays(-number), DateTime.Now, GroupBy);
+                case 1:
+                    return GetNumberOfExportedBook(DateTime.Now.AddMonths(-number), DateTime.Now, GroupBy);
+                case 2:
+                    return GetNumberOfExportedBook(DateTime.Now.AddYears(-number), DateTime.Now, GroupBy);
+            }
+            return null;
+        }
     }
 }
