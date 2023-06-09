ea4e7137ffdb4915ebd7443682a79acc7a971fab
Mạnh Hoàng Đức <hoangducmanh254@gmail.com>
2023-06-09 18:03:46 +0700

    publisher export: done

--- BPMS/GUI/Publisher/FormPublisherExport.Designer.cs
+++ BPMS/GUI/Publisher/FormPublisherExport.Designer.cs
@@ -28,19 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dtgvImport = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DisplayName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ImportDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DeliveryPerson = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnitLeader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvImport)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dtgvImport
+            // 
+            this.dtgvImport.AllowUserToAddRows = false;
+            this.dtgvImport.AllowUserToDeleteRows = false;
+            this.dtgvImport.AllowUserToResizeColumns = false;
+            this.dtgvImport.AllowUserToResizeRows = false;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            this.dtgvImport.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dtgvImport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(165)))));
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 12F);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(130)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgvImport.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dtgvImport.ColumnHeadersHeight = 35;
+            this.dtgvImport.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.DisplayName,
+            this.ImportDate,
+            this.DeliveryPerson,
+            this.UnitLeader});
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgvImport.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dtgvImport.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(130)))));
+            this.dtgvImport.Location = new System.Drawing.Point(12, 10);
+            this.dtgvImport.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.dtgvImport.MultiSelect = false;
+            this.dtgvImport.Name = "dtgvImport";
+            this.dtgvImport.ReadOnly = true;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.Padding = new System.Windows.Forms.Padding(2);
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgvImport.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.dtgvImport.RowHeadersVisible = false;
+            this.dtgvImport.RowHeadersWidth = 62;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtgvImport.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            this.dtgvImport.RowTemplate.Height = 37;
+            this.dtgvImport.Size = new System.Drawing.Size(1170, 640);
+            this.dtgvImport.TabIndex = 1;
+            this.dtgvImport.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dtgvImport.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dtgvImport.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dtgvImport.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dtgvImport.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dtgvImport.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dtgvImport.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(130)))));
+            this.dtgvImport.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dtgvImport.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dtgvImport.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtgvImport.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dtgvImport.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dtgvImport.ThemeStyle.HeaderStyle.Height = 35;
+            this.dtgvImport.ThemeStyle.ReadOnly = true;
+            this.dtgvImport.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dtgvImport.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dtgvImport.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtgvImport.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dtgvImport.ThemeStyle.RowsStyle.Height = 37;
+            this.dtgvImport.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dtgvImport.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "Import ID";
+            this.ID.MinimumWidth = 8;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // DisplayName
+            // 
+            this.DisplayName.HeaderText = "Publisher";
+            this.DisplayName.MinimumWidth = 8;
+            this.DisplayName.Name = "DisplayName";
+            this.DisplayName.ReadOnly = true;
+            // 
+            // ImportDate
+            // 
+            this.ImportDate.HeaderText = "Import Date";
+            this.ImportDate.MinimumWidth = 8;
+            this.ImportDate.Name = "ImportDate";
+            this.ImportDate.ReadOnly = true;
+            // 
+            // DeliveryPerson
+            // 
+            this.DeliveryPerson.HeaderText = "Delivery Person";
+            this.DeliveryPerson.MinimumWidth = 8;
+            this.DeliveryPerson.Name = "DeliveryPerson";
+            this.DeliveryPerson.ReadOnly = true;
+            // 
+            // UnitLeader
+            // 
+            this.UnitLeader.HeaderText = "Unit Leader";
+            this.UnitLeader.MinimumWidth = 8;
+            this.UnitLeader.Name = "UnitLeader";
+            this.UnitLeader.ReadOnly = true;
             // 
             // FormPublisherExport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(990, 452);
+            this.ClientSize = new System.Drawing.Size(1194, 660);
+            this.Controls.Add(this.dtgvImport);
             this.Name = "FormPublisherExport";
             this.Text = "FormPublisherExport";
+            this.ParentChanged += new System.EventHandler(this.FormPublisherExport_ParentChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvImport)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2DataGridView dtgvImport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DisplayName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ImportDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DeliveryPerson;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnitLeader;
     }
 }
--- BPMS/GUI/Publisher/FormPublisherExport.cs
+++ BPMS/GUI/Publisher/FormPublisherExport.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BPMS.Classes;
+using BPMS.DAO;
+using BPMS.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,54 @@ namespace BPMS.GUI.Publisher
 {
     public partial class FormPublisherExport : Form
     {
+        DTO.Publisher currentPublisher = null;
         public FormPublisherExport()
         {
             InitializeComponent();
+        }
+        private int GetAccountInfo()
+        {
+            Control c = this;
+            while (c.GetType() != typeof(FormMainMenu))
+            {
+                c = c.Parent;
+                if (c == null) return -1;
+            }
+            if (c != null)
+            {
+                Account tmp = AccountDAO.Instance.GetAccount((c as FormMainMenu).AccountId);
+                if (tmp != null)
+                {
+                    if ((Permissions)tmp.type == Permissions.Admin)
+                    {
+                        currentPublisher = null;
+                    }
+                    else
+                    {
+                        currentPublisher = tmp.Publishers.FirstOrDefault();
+                    }
+                }
+            }
+            return 0;
+        }
+        void LoadData()
+        {
+            List<ImportReport> listEr = ImportReportDAO.Instance.GetListImportBill(currentPublisher);
+            dtgvImport.Rows.Clear();
+            foreach (ImportReport ir in listEr)
+            {
+                dtgvImport.Rows.Add(new object[] { ir.id
+                                                    , ir.Publisher.Account.DisplayName
+                                                    , ir.ImportDate
+                                                    , ir.DeliveryPerson
+                                                    , ir.UnitLeader});
+            }
+        }
+
+        private void FormPublisherExport_ParentChanged(object sender, EventArgs e)
+        {
+            if (GetAccountInfo() == -1) return;
+            LoadData();
         }
     }
 }
